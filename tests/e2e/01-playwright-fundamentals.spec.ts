a46380365988d3176ee6c5ea52cba9be93ecb523
Kris <mr.octopus01@gmail.com>
2026-05-26 14:27:54 +0100

    ex1

--- tests/e2e/01-playwright-fundamentals.spec.ts
+++ tests/e2e/01-playwright-fundamentals.spec.ts
@@ -6,9 +6,10 @@ test.describe('Fundamentals test suite', () => {
   });
   /**
    *Exercise 1: debug the test below and make it pass
-   */
+  //  */
   //   test('ex 1: the user should be able to logout', async ({ loginPage, page }) => {
   //     await loginPage.login('admin@practicesoftwaretesting.com', 'welcome01');
+  //     await page.getByTestId('nav-menu').click();
   //     await page.locator('[data-test="nav-sign-out"]').click();
   //     await expect(page.locator('[data-test="nav-sign-in"]')).toContainText('Sign in');
   //   });
@@ -16,13 +17,13 @@ test.describe('Fundamentals test suite', () => {
   /**
    * Exercise 2: recreate the logout scenario, using Page Object Model design pattern
    */
-  // test('ex 2: the user should be able to logout - using POM design pattern', async ({
-  //   loginPage,
-  //   headerPage,
-  // }) => {
-  //   await loginPage.login('admin@practicesoftwaretesting.com', 'welcome01');
+  test('ex 2: the user should be able to logout - using POM design pattern', async ({
+    loginPage,
+    headerPage,
+  }) => {
+    await loginPage.login('admin@practicesoftwaretesting.com', 'welcome01');
 
-  // });
+  });
 
   /**
    * Quality checkpoint:
